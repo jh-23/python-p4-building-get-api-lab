bd3e3a7931467cc8b7104b9db4007c91de46aa50
John Heger <heger7@gmail.com>
2024-04-20 09:39:38 -0500

    completed lab

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,56 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    
+    response = make_response(
+        bakeries,
+        200
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    
+    bakery_dict = bakery.to_dict()
+    
+    response = make_response(
+        bakery_dict,
+        200
+    )
+    
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    
+    baked_goods_price_sorted = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    
+    json_baked_goods = [baked_good.to_dict() for baked_good in baked_goods_price_sorted]
+    
+    response = make_response(
+        json_baked_goods,
+        200
+    )
+    
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    
+    json_most_expensive = most_expensive.to_dict()
+
+    response = make_response(
+        jsonify(json_most_expensive),
+        200
+    )    
+    
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
